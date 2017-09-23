b0515eda04dac6d2be716e173ba4c53349d2fb32
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-09-23 18:49:51 +0200

    initial commit

---
+++ 01-electronFileSystem/index.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, globalShortcut, Menu } = require('electron');
+
+const path = require('path');
+const url = require('url');
+
+let mainWindow = null;
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', () => {
+
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768,
+  });
+
+  // console.log("from main process --> " + __dirname);
+  console.log(`from main process --> ${__dirname}`);
+
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+
+  globalShortcut.register('CmdOrCtrl+Shift+i', () => {
+    mainWindow.webContents.toggleDevTools();
+  });
+});
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
---
+++ 01-electronFileSystem/renderer.js
@@ -0,0 +1,8 @@
+console.log("from renderer...");
+
+const fs = require('fs');
+const os = require('os');
+
+const files = fs.readdirSync(os.homedir());
+
+document.getElementById("temp").innerHTML = files.join('<br/>');
